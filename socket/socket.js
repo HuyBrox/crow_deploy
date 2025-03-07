2ebce96ede9ab05300381ecbe2f19d5febd347d2
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2025-03-07 22:02:56 +0700

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 const io = new Server(server, {
     cors: {
-        origin: `http://localhost:${PORT}`,
+        origin: `https://crow-r6s9.onrender.com`,
         methods: ["GET", "POST"],
     },
     pingInterval: 25000,
