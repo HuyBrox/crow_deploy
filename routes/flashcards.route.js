681908543956ea9850ed3923cadd173fa74c5ecb
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2025-04-13 22:17:05 +0700

    Update flashcards.route.js

--- routes/flashcards.route.js
+++ routes/flashcards.route.js
@@ -11,5 +11,5 @@ router.get('/flashcards/createCard', requireAuth, getCreateCard);
 router.post('/flashcards/createCard', requireAuth, postCreateCard);
 router.delete('/flashcards/delete/:id', requireAuth, deleteFlashCard);
 router.get('/review/:id', requireAuth, baiTapTuVung);
-router.get('/flashcards/list/:id', requireAuth, );
+router.get('/flashcards/list/:id', requireAuth, getList);
 export default router;
